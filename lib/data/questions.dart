930c52000aeb2a64d2f87b29a6fae3a8d5d22d33
MikelWinemi <124489963+MikelWinemi@users.noreply.github.com>
2023-11-16 23:24:52 -0500

    initial commit

---
+++ lib/data/questions.dart
@@ -0,0 +1,55 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'How many titles has Lionel Messi won?',
+    [
+      '44',
+      '23',
+      '19',
+      '50',
+    ],
+  ),
+  QuizQuestion('How many goals did Ronaldo score for Real Madrid', [
+    '450',
+    '289',
+    '380',
+    '600',
+  ]),
+  QuizQuestion(
+    'Which team has the most UCLs amongst these teams?',
+    [
+      'AC Milan',
+      'Ajax',
+      'Celtics',
+      'Benfica',
+    ],
+  ),
+  QuizQuestion(
+    'Which team has the most world cups?',
+    [
+      'Brazil',
+      'France',
+      'Germany',
+      'Italy',
+    ],
+  ),
+  QuizQuestion(
+    'Most Premier league assists in history blongs to?',
+    [
+      'Ryan Giggs',
+      'Stevan Gerrad',
+      'Dennis Bergkamp',
+      'Kevin De Bruyne',
+    ],
+  ),
+  QuizQuestion(
+    'Who has the most world cup goals in total?',
+    [
+      'Klose',
+      'Just Fontaine',
+      'Mbappe',
+      'Pele'
+       ],
+  ),
+];
